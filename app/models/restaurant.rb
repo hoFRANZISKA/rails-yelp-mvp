39ec54d20f36f2c0c51859158518047fd47c4f9e
Franziska Horn <Franziska_Horn@hotmail.com>
2024-05-15 12:32:23 +0200

    Finished validation in models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  validates :name, :address, :category, presence: true
+  @allowed_categories = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates_inclusion_of :category, in: @allowed_categories
+  has_many :reviews, dependent: :destroy
 end
